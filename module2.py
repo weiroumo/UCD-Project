e2e680af1c32f8d591575025d25af2431af04bc7
moweirou <weirou.mo@ucdconnect.ie>
2021-03-14 01:31:53 +0000

    subsetting list

--- module2.py
+++ module2.py
@@ -6,7 +6,7 @@ bed = 10.75
 bath = 9.50
 
 # Create list areas
-areas = [hall, kit, liv, bed, bath ]
+areas = [hall, kit, liv, bed, bath]
 
 # Print areas
 print(areas)
@@ -18,8 +18,41 @@ liv = 20.0
 bed = 10.75
 bath = 9.50
 
+
 # Adapt list areas
 areas = ["hallway", hall, "kitchen", kit, "living room", liv, "bedroom", bed, "bathroom", bath]
 
 # Print areas
 print(areas)
+
+
+# Print out second element from areas
+print(areas[1])
+
+# Print out last element from areas
+print(areas[-1])
+
+# Print out the area of the living room
+print(areas[5])
+
+# Sum of kitchen and bedroom area: eat_sleep_area
+eat_sleep_area = areas[3]+areas[7]
+
+# Print the variable eat_sleep_area
+print(eat_sleep_area)
+
+# Use slicing to create downstairs
+downstairs = areas[0:6]
+
+# Use slicing to create upstairs
+upstairs = areas[6:11]
+
+# Print out downstairs and upstairs
+print(downstairs)
+print(upstairs)
+
+# Alternative slicing to create downstairs
+downstairs = areas[:6]
+
+# Alternative slicing to create upstairs
+upstairs = areas[6:]
