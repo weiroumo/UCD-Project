c9c5afaa3064cabe6d63de70d4db7ba806eb3f72
moweirou <weirou.mo@ucdconnect.ie>
2021-03-15 00:12:30 +0000

    add list manipulation

--- module2.py
+++ module2.py
@@ -56,3 +56,43 @@ downstairs = areas[:6]
 
 # Alternative slicing to create upstairs
 upstairs = areas[6:]
+
+# list manipulation
+
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Correct the bathroom area
+areas[9] = 10.50
+
+# Change "living room" to "chill zone"
+areas[4] = "chill zone"
+
+# Create the areas list and make some changes
+areas = ["hallway", 11.25, "kitchen", 18.0, "chill zone", 20.0,
+         "bedroom", 10.75, "bathroom", 10.50]
+
+# Add poolhouse data to areas, new list is areas_1
+areas_1 = areas + ["poolhouse", 24.5]
+
+# Add garage data to areas_1, new list is areas_2
+areas_2 = areas_1 + ["garage", 15.45]
+
+areas = ["hallway", 11.25, "kitchen", 18.0,
+        "chill zone", 20.0, "bedroom", 10.75,
+         "bathroom", 10.50, "poolhouse", 24.5,
+         "garage", 15.45]
+del(areas[-4:-2])
+print(areas)
+
+# Create list areas
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Create areas_copy using list to prevent changes in areas_copy from also taking effect in areas
+areas_copy = list(areas)
+
+# Change areas_copy
+areas_copy[0] = 5.0
+
+# Print areas
+print(areas)
