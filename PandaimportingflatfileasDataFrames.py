854ed16e079e75eb3b776dbc9eba99b8b296250c
moweirou <weirou.mo@ucdconnect.ie>
2021-04-10 22:09:18 +0100

    working with mixed data types

---
+++ PandaimportingflatfileasDataFrames.py
@@ -0,0 +1,44 @@
+# Import pandas as pd
+import pandas as pd
+
+# Assign the filename: file
+file = 'titanic.csv'
+
+# Read the file into a DataFrame: df
+df = pd.read_csv(file)
+
+pd.set_option('max_columns', None)
+pd.set_option('max_rows', None)
+
+# View the head of the DataFrame
+print(df.head())
+
+#Assign the filename: file
+file = 'mnist_kaggle_some_rows.csv'
+
+# Read the first 5 rows of the file into a DataFrame: data
+data = pd.read_csv(file, nrows=5, header= None)
+
+# Build a numpy array from the DataFrame: data_array
+data_array = data.values
+
+# Print the datatype of data_array to the shell
+print(type(data_array))
+
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Assign filename: file
+file = 'titanic_corrupt.txt'
+
+# Import file: data
+data = pd.read_csv(file, sep='\t', comment='#', na_values=['Nothing'])
+
+# Print the head of the DataFrame
+print(data.head())
+
+# Plot 'Age' variable in a histogram
+pd.DataFrame.hist(data[['Age']])
+plt.xlabel('Age (years)')
+plt.ylabel('count')
+plt.show()
