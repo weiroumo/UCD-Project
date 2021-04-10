32d0c712c7208283f29441cce6358c9e9c5155e6
moweirou <weirou.mo@ucdconnect.ie>
2021-04-10 15:02:30 +0100

    working with mixed data types

--- importingFlatFilewithNumpy.py
+++ importingFlatFilewithNumpy.py
@@ -30,3 +30,46 @@ data = np.loadtxt(file, delimiter='\t', skiprows= 1, usecols=[0,2])
 # Print data
 print(data)
 
+# Assign filename: file
+file = 'seaslug.txt'
+
+# Import file: data
+data = np.loadtxt(file, delimiter='\t', dtype=str)
+
+# Print the first element of data
+print(data[0])
+
+# Import data as floats and skip the first row: data_float
+data_float = np.loadtxt(file, delimiter='\t', dtype=float, skiprows=1)
+
+# Print the 10th element of data_float
+print(data_float[9])
+
+# Plot a scatterplot of the data
+plt.scatter(data_float[:, 0], data_float[:, 1])
+plt.xlabel('time (min.)')
+plt.ylabel('percentage of larvae')
+plt.show()
+
+# working with mixed data types
+# the first argument is the filename,
+# the second specifies the delimiter ,
+# the third argument names tells us there is a header.
+data = np.genfromtxt('titanic.csv', delimiter=',', names=True, dtype=None)
+np.shape(data)
+
+#to get the ith row, merely execute data[i]
+#get the column with name 'Fare'
+data['Fare']
+
+# another way to work with mixed data types
+# Assign the filename: file
+file = 'titanic.csv'
+
+# Import file using np.recfromcsv: d
+d = np.recfromcsv(file, delimiter=',', names=True, dtype=None)
+
+# Print out first three entries of d
+print(d[:3])
+
+
