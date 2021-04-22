23a115d313859832fc80685209bee09ea70df2a4
moweirou <weirou.mo@ucdconnect.ie>
2021-04-22 18:28:39 +0100

    add filtering data

--- TransformingData.py
+++ TransformingData.py
@@ -43,3 +43,41 @@ homelessness_reg_fam = homelessness.sort_values(["region", "family_members"], as
 
 # Print the top few rows
 print(homelessness_reg_fam.head())
+
+# Select the individuals column
+individuals = homelessness["individuals"]
+
+# Print the head of the result
+print(individuals.head())
+
+# Select the state and family_members columns
+state_fam = homelessness[["state", "family_members"]]
+
+# Print the head of the result
+print(state_fam.head())
+
+# Select only the individuals and state columns, in that order
+ind_state = homelessness[["individuals","state"]]
+
+# Print the head of the result
+print(ind_state.head())
+
+# Filter for rows where individuals is greater than 10000
+ind_gt_10k = homelessness[homelessness["individuals"] > 10000]
+
+# See the result
+print(ind_gt_10k)
+
+# Filter for rows where region is Mountain
+mountain_reg = homelessness[homelessness["region"] == "Mountain"]
+
+# See the result
+print(mountain_reg)
+
+# Filter for rows where family_members is less than 1000
+# and region is Pacific
+fam_lt_1k_pac = homelessness[(homelessness["family_members"] < 1000) & (homelessness["region"] == "Pacific")]
+
+# See the result
+print(fam_lt_1k_pac)
+
