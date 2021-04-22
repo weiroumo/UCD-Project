d1adedb3395c6162f5d33604c833da8ea3a841e5
moweirou <weirou.mo@ucdconnect.ie>
2021-04-22 22:24:38 +0100

    added subset data and adding new columns

--- TransformingData.py
+++ TransformingData.py
@@ -1,6 +1,6 @@
 import pandas as pd
 file = "homelessness.csv"
-pd.read_csv(file)
+homelessness = pd.read_csv(file)
 
 # Print the head of the homelessness data
 print(homelessness.head())
@@ -81,3 +81,44 @@ fam_lt_1k_pac = homelessness[(homelessness["family_members"] < 1000) & (homeless
 # See the result
 print(fam_lt_1k_pac)
 
+# Subset for rows in South Atlantic or Mid-Atlantic regions
+south_mid_atlantic = homelessness[(homelessness["region"] == "South Atlantic") | (homelessness["region"] == "Mid-Atlantic")]
+
+# See the result
+print(south_mid_atlantic)
+
+# Conditional subset data
+# The Mojave Desert states
+canu = ["California", "Arizona", "Nevada", "Utah"]
+
+# Filter for rows in the Mojave Desert states
+mojave_homelessness = homelessness[homelessness["state"].isin(canu)]
+
+# See the result
+print(mojave_homelessness)
+
+## Adding new columns to dataset
+# Add total col as sum of individuals and family_members
+homelessness["total"] = homelessness["individuals"] + homelessness["family_members"]
+
+# Add p_individuals col as proportion of individuals
+homelessness["p_individuals"] = homelessness["individuals"] / homelessness["total"]
+
+# See the result
+print(homelessness)
+
+## Combo
+# Create indiv_per_10k col as homeless individuals per 10k state pop
+homelessness["indiv_per_10k"] = 10000 * homelessness["individuals"] / homelessness["state_pop"]
+
+# Subset rows for indiv_per_10k greater than 20
+high_homelessness = homelessness[homelessness["indiv_per_10k"] > 20]
+
+# Sort high_homelessness by descending indiv_per_10k
+high_homelessness_srt = high_homelessness.sort_values("indiv_per_10k", ascending = False)
+
+# From high_homelessness_srt, select the state and indiv_per_10k cols
+result = high_homelessness_srt[["state", "indiv_per_10k"]]
+
+# See the result
+print(result)
